5eff1552eb589010f2c41e360d75f825430ccdcb
FAYZKAAP <fayzulloxxakimov574@gmail.com>
2025-06-10 09:54:14 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Xakimov Fayzulloh</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam blanditiis tempore veritatis error. Numquam quaerat at minima, debitis molestias delectus officia. Perspiciatis sit labore minus animi veniam, ratione voluptatem fugiat?</p>
+      <ul>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
